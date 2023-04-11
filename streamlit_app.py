57fa82b0ffc1af293ccbb76e658cdbb4882957e3
mayur1234567890 <83023234+mayur1234567890@users.noreply.github.com>
2023-04-11 17:33:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('This is my first application!')
+
+streamlit.header('This is my first application!')
+
+streamlit.text('This is my first application!')
+streamlit.text('This is my first application!')
+streamlit.text('This is my first application!')
