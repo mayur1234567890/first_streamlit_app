1774f95b2bd723b38ddf160e63a5d32a45168324
mayur1234567890 <83023234+mayur1234567890@users.noreply.github.com>
2023-04-11 17:46:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
 import streamlit
 
-streamlit.title('This is my first application!')
+streamlit.title('My Mom's New Healthy Diner')
 
-streamlit.header('Find the List Below...')
 
-streamlit.text('Mango')
-streamlit.text('Banana')
-streamlit.text('Grapes')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
